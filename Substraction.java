e3d4c0ea3d553936ae422ba525baabcbae2ad107
Pranoti Kharade <151639522+Kharade-Pranoti@users.noreply.github.com>
2023-11-22 14:40:55 +0530

    Create Substraction.java

---
+++ Substraction.java
@@ -0,0 +1,20 @@
+// A program to do substraction of two numbers
+
+import java.util.Scanner;
+
+public class Sub
+  {
+    public static void main(String args[])
+    {
+      Scanner sc = new Scanner(System.in);
+
+      System.out.println("Enter first number");
+      int a = sc.nextInt();
+
+      System.out.println("Enter second number");
+      int b = sc.nextInt();
+
+      int sub = a - b;
+      System.out.println("substraction = " + sub);
+    }
+  }
