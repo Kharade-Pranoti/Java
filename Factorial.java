5a4313ce7000bf33788f463409277b3bf7965623
Pranoti Kharade <151639522+Kharade-Pranoti@users.noreply.github.com>
2023-12-13 17:40:19 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,22 @@
+// A program to find the factorial of a number
+
+import java.util.*;
+
+public class Factorial{
+  public static void main(String args []){
+    int n;
+    int fact = 1;
+    
+    System.out.println("Enter any number to find it's factorial.");
+
+    // taking user input
+    Scanner sc = new Scanner(System.in);
+    n = sc.nextInt();
+
+    for(int i = 1; i < n; i++){
+      fact = fact * i;
+    }
+
+    System.out.println("Factorial is : " +fact);
+  }
+}
