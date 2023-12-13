f323b07a39e39cf0fced1d11674321b25f2bccac
Pranoti Kharade <151639522+Kharade-Pranoti@users.noreply.github.com>
2023-12-13 17:16:36 +0530

    Create Max_Num.java

---
+++ Max_Num.java
@@ -0,0 +1,24 @@
+// A program to find the maximum number 
+
+class Max_Num {
+	public static void main (String[] args) {
+	   int num1 , num2 , max ;
+	   
+	   num1 = 563;
+	   num2 = 536;
+	  
+	  // condition to check max number 
+	   if(num1 > num2)
+	   {
+	       max = num1;
+	   }
+	   else
+	   {
+	       max = num2;
+	   }
+
+     // printing the maximum number
+	   System.out.println("The maximum number among two numbers is "+max);
+	   
+	}   
+}
