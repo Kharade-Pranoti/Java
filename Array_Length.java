24db12dcc518a20ec5bdbe0084c6dd1bb424ae7f
Pranoti Kharade <151639522+Kharade-Pranoti@users.noreply.github.com>
2023-12-20 16:51:15 +0530

    Create Array_Length.java

---
+++ Array_Length.java
@@ -0,0 +1,12 @@
+// Find the length of an array
+
+public class Array_Length{
+  public static void main(String args [])
+  {
+      int arr[] = new int[4];
+      
+      // to find the length of an array we have length function
+      System.out.println("Array size is "+ arr.length); // 4
+  }
+  
+}
