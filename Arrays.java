37d6c2a808f6bdbfbc5f19fc093d2e7a07a7b29d
Pranoti Kharade <151639522+Kharade-Pranoti@users.noreply.github.com>
2023-12-20 16:46:55 +0530

    Create Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,16 @@
+// Creating an array and initializing it with values. Print all the elements in the array
+
+public class Arrays{
+  public static void main(String args[]){
+    
+    // declaring and initializing an array
+    int arr[] = new int[] {22, 33, 44, 55, 66};
+
+    // printing the elements of an array
+    for(int i = 0; i < 5; i++)
+      {
+        System.out.println(arr[i] + " ");
+      }
+  
+  }
+}
